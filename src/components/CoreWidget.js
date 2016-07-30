3f389317835a158d271460e509f56f8d24ae4732
Michael Hoang <michael.hoang@wspdigital.com>
2016-07-30 21:59:47 +1000

    Change width stuff

--- src/components/CoreWidget.js
+++ src/components/CoreWidget.js
@@ -10,14 +10,14 @@ export default class QotDWidget extends React.Component {
 
   static id = 'quote-of-the-day';
   static widgetName = 'Quote of the Day';
-  static sizes = [[1, 1], [2, 1]];
+  static sizes = [[2, 1]];
 
   render() {
     return (
       <div className="uk-margin-left uk-vertical-align container uk-margin-right">
         <div className="uk-vertical-align-middle">
-          <blockquote>
-            <i className="uk-icon-medium uk-icon-quote-left uk-margin-small-right"></i>
+          <blockquote className="quote">
+            <i className="uk-icon-medium uk-icon-quote-left uk-margin-small-right quote-text"></i>
             Victorious warriors win first and then go to war, while defeated warriors go to war first and then seek to win.
             <i className="uk-icon-medium uk-icon-quote-right uk-margin-small-left"></i>
           </blockquote>
